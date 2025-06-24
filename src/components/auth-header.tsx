08e62579ba8d631baa1c85ffff4bdfd51509f151
mattéo magnani <matteojulienmagnani@gmail.com>
2025-06-24 12:53:08 +0000

    corrige l'icone de tiktok

--- src/components/auth-header.tsx
+++ src/components/auth-header.tsx
@@ -33,10 +33,13 @@ export function AuthHeader() {
             height={32}
           />
         </Link>
-        <Link href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
-           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 1200 1227" fill="currentColor">
-                <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6902H306.615L611.412 515.685L658.88 583.579L1055.08 1150.31H892.476L569.165 687.854V687.828Z"/>
-           </svg>
+        <Link href="https://www.tiktok.com/" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
+          <Image
+            src="https://static.vecteezy.com/system/resources/previews/023/986/704/original/tiktok-logo-tiktok-logo-transparent-tiktok-icon-transparent-free-free-png.png"
+            alt="TikTok"
+            width={32}
+            height={32}
+          />
         </Link>
       </div>
     </div>
